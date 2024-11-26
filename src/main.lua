ade1cf98b7d1d4ab1798145c56554d2b03bf7f61
joseareia <jose.apareia@gmail.com>
2024-11-26 11:38:38 +0000

    patch: deletion of deprecated variables and simply the deletion method

--- src/main.lua
+++ src/main.lua
@@ -1,50 +1,48 @@
 local utils = require("src.utils")
 
--- TODO: Deprecate these variables and the logic behind them. Does not make any sense!
-local is_installed = nil
-local app_name = 'spotify'
-local username = utils.get_username()
+-- Local $HOME variable environment.
+local home_env = os.getenv("HOME")
 
 -- Global variable that can be alter in the future.
-cache_path = '/home/' .. username .. '/.cache/spotify/Storage'
+cache_path = home_env .. '/.cache/spotify/Storage'
+data_path = home_env .. '/.cache/spotify/Data'
 
--- Check if directory exists. 
--- If not (default logic) a try-out to get into a custom setting file will be done.
+-- Check if directory exists. If not (default logic) a try-out to get into a custom file setting will be done.
 if utils.directory_exists(cache_path) == nil then
     local config_env = {}
-    local home_env = os.getenv("HOME")
     local f, err = loadfile(home_env .. "/.local/share/spotify-cleaner/settings.conf", "t", config_env)
 
     if f then
         f()
-        cache_path = config_env.user_settings.path
-        print("Loadfile " .. cache_path)
+        cache_path = config_env.user_settings.cache_path
+        data_path = config_env.user_settings.data_path
     else
         print(err)
     end
 end
 
--- Check is Spotify is installed.
-local handle = io.popen('which ' .. app_name)
-if not handle then
-    print("[ ERROR ] The command failed to execute.")
-else
-    local result = handle:read("*a")
-    handle:close()
-    if result ~= "" then is_installed = true end
+-- Checks the cache size. If the cache exceeds 100MB, it is deleted.
+local cache_exists = utils.directory_exists(cache_path)
+if cache_exists then cache_size = utils.get_directory_size(cache_path) end   
+cache_size = tonumber(string.format("%.2f", cache_size / 1024)) -- Convert from kB to MB.
+
+if cache_size >= 100 then
+    local cmd_delete_dir = "rm -r " .. cache_path .. "/*"
+    local ok, exit = os.execute(cmd_delete_dir)
+    if ok ~= true then
+        print("[ ERROR ] Could not delete the directory due to: '" .. exit .. "'.")
+    end
 end
 
--- If Spotify is installed, checks the cache size. If the cache exceeds 100MB, it is deleted.
-if is_installed then
-    local cache_exists = utils.directory_exists(cache_path)
-    if cache_exists then cache_size = utils.get_directory_size(cache_path) end   
-    cache_size = tonumber(string.format("%.2f", cache_size / 1024)) -- Convert from kB to MB.
+-- Checks the data size. If the data exceeds 100MB, it is deleted.
+local data_exists = utils.directory_exists(data_path)
+if data_exists then data_size = utils.get_directory_size(data_path) end
+data_size = tonumber(string.format("%.2f", data_size / 1024)) -- Convert from kB to MB.
 
-    if cache_size >= 100 then
-        local cmd_delete_dir = "rm -r " .. cache_path .. "/*"
-        local ok, exit = os.execute(cmd_delete_dir)
-        if ok ~= true then
-            print("[ ERROR ] Could not delete the directory due to: '" .. exit .. "'.")
-        end
+if data_size >= 100 then
+    local cmd_delete_dir = "rm -r " .. data_path .. "/*"
+    local ok, exit = os.execute(cmd_delete_dir)
+    if ok ~= true then
+        print("[ ERROR ] Could not delete the directory due to: '" .. exit .. "'.")
     end
 end
