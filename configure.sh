74b63319bc7293fd6b324e022ad1c083b1264cae
joseareia <jose.apareia@gmail.com>
2024-11-26 14:21:57 +0000

    patch: new functions to clean the working directory and error handling

--- configure.sh
+++ configure.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
 
-APP_PATH="$HOME/.cache/spotify/Storage"
-CONF_FILE="$HOME/.local/share/spotify-cleaner/settings.conf"
-CONF_DIR="$(dirname "$CONF_FILE")" 
+APP_CACHE_DIR="$HOME/.cache/spotify/Storage"
+CONFIG_FILE="$HOME/.local/share/spotify-cleaner/settings.conf"
+CONFIG_DIR="$(dirname "$CONFIG_FILE")"
 
+# Colors for output.
 CLR_GREEN="\033[92m"
 CLR_RED="\033[91m"
 CLR_YELLOW="\033[93m"
@@ -12,111 +14,138 @@ CLR_RESET="\033[0m"
 OK="[ ${CLR_GREEN}OK${CLR_RESET} ]"
 NOK="[ ${CLR_RED}NOK${CLR_RESET} ]"
 
-check_make() {
-    if ! command -v make 2>&1 >/dev/null; then
-        echo -e "${NOK} Make is not installed. Installing it..."
-        sudo apt -y install make gcc build-essential
-        echo -e "${OK} Installation completed for: make."
+# Clean up temporary files on exit.
+cleanup() {
+    rm -f lua-5.3.5.tar.gz luarocks-3.11.1.tar.gz
+}
+trap cleanup EXIT
+
+# Check and install a given package.
+check_and_install() {
+    local package="$1"
+    if ! command -v "$package" >/dev/null 2>&1; then
+        echo -e "${NOK} The package ${CLR_YELLOW}$package${CLR_RESET} is not installed. Installing."
+        sudo apt-get -y update && sudo apt-get -y install "$package" "build-essential"
+        echo -e "${OK} The package ${CLR_YELLOW}$package${CLR_RESET} was installed successfully."
     else
-        echo -e "${OK} ${CLR_YELLOW}Make${CLR_RESET} is installed."
+        echo -e "${OK} The package ${CLR_YELLOW}$package${CLR_RESET} is already installed."
     fi
 }
 
+# Download and extract tar compressed files.
+download_and_extract() {
+    local url="$1"
+    local tar_file="$2"
+    echo -e "${OK} Downloading ${CLR_YELLOW}$tar_file${CLR_RESET}."
+    curl -R -O "$url"
+    tar -zxf "$tar_file"
+    cd "${tar_file%.tar.gz}" || exit
+}
+
+check_make() {
+    check_and_install "make"
+}
+
+check_gcc() {
+    check_and_install "gcc"
+}
+
 check_lua() {
-    if ! command -v lua 2>&1 >/dev/null; then
-        echo -e "${NOK} Lua is not installed. Installing it..."
-        curl -R -O http://www.lua.org/ftp/lua-5.3.5.tar.gz
-        tar -zxf lua-5.3.5.tar.gz; cd lua-5.3.5
+    if ! command -v lua >/dev/null 2>&1; then
+        echo -e "${NOK} The package ${CLR_YELLOW}lua${CLR_RESET} is not installed. Installing it."
+        download_and_extract "http://www.lua.org/ftp/lua-5.3.5.tar.gz" "lua-5.3.5.tar.gz"
         make linux test
         sudo make install
-        echo -e "${OK} Installation completed for 'lua'."
+        echo -e "${OK} The package ${CLR_YELLOW}lua${CLR_RESET} installed successfully."
+        cd ..
     else
-        echo -e "${OK} ${CLR_YELLOW}Lua${CLR_RESET} is installed."
+        echo -e "${OK} The package ${CLR_YELLOW}lua${CLR_RESET} is already installed."
     fi
 }
 
 check_luarocks() {
-    if ! command -v luarocks 2>&1 >/dev/null; then
-        echo -e "${NOK} Luarocks is not installed. Installing it..."
-        curl -R -O http://luarocks.github.io/luarocks/releases/luarocks-3.11.1.tar.gz
-        tar -zxf luarocks-3.11.1.tar.gz; cd luarocks-3.11.1
+    if ! command -v luarocks >/dev/null 2>&1; then
+        echo -e "${NOK} The package ${CLR_YELLOW}luarocks${CLR_RESET} is not installed. Installing it."
+        download_and_extract "http://luarocks.github.io/luarocks/releases/luarocks-3.11.1.tar.gz" "luarocks-3.11.1.tar.gz"
         ./configure --with-lua-include=/usr/local/include
         make
         sudo make install
-        echo -e "${OK} Installation completed for 'luarocks'."
+        echo -e "${OK} The package ${CLR_YELLOW}luarocks${CLR_RESET} was installed successfully."
+        cd ..
     else
-        echo -e "${OK} ${CLR_YELLOW}Luarocks${CLR_RESET} is installed."
+        echo -e "${OK} The package ${CLR_YELLOW}luarocks${CLR_RESET} is already installed."
     fi
 }
 
 check_luastatic() {
-    if ! command -v luastatic 2>&1 >/dev/null; then
-        echo -e "${NOK} Luastatic is not installed. Installing it..."
+    if ! command -v luastatic >/dev/null 2>&1; then
+        echo -e "${NOK} The package ${CLR_YELLOW}luastatic${CLR_RESET} is not installed. Installing it via Luarocks."
         luarocks install luastatic
-        echo -e "${OK} Installation completed for 'luastatic'."
+        echo -e "${OK} The package ${CLR_YELLOW}luastatic${CLR_RESET} was installed successfully."
     else
-        echo -e "${OK} ${CLR_YELLOW}Luastatic${CLR_RESET} is installed."
+        echo -e "${OK} The package ${CLR_YELLOW}luastatic${CLR_RESET} is already installed."
     fi
 }
 
-# Check if the app cache directory exists.
-check_directory() {
-    if [ -d "$APP_PATH" ]; then
-        echo -e "${OK} The default cache directory exists. Exiting."
+check_cache_directory() {
+    if [ -d "$APP_CACHE_DIR" ]; then
+        echo -e "${OK} Default cache directory exists."
     else
-        echo -e "${NOK} The default cache directory does not exist."
-        prompt_user
+        echo -e "${NOK} Default cache directory does not exist."
+        prompt_user_for_directory
     fi
 }
 
-# Ensure that the configuration directory exists.
+# Create configuration directory if it doesn't exist
 ensure_config_directory() {
-    if [ ! -d "$CONF_DIR" ]; then
-        echo -e "${OK} The configuration directory does not exist. Creating it..."
-        mkdir -p "$CONF_DIR"
-        if [ $? -eq 0 ]; then
-            echo -e "${OK} Configuration directory created successfully."
-        else
-            echo -e "${NOK} Failed to create directory '$CONF_DIR'. Exiting."
-            exit 1
-        fi
+    if [ ! -d "$CONFIG_DIR" ]; then
+        echo -e "${OK} Configuration directory does not exist. Creating it."
+        mkdir -p "$CONFIG_DIR"
+        echo -e "${OK} Configuration directory created."
     fi
 }
 
-prompt_user() {
+# Prompt user to specify a directory
+prompt_user_for_directory() {
     while true; do
-        read -p "Please specify a valid directory for the storage cache: " user_input
+        read -rp "Please specify a valid directory for the storage cache: " user_input
         if [ -d "$user_input" ]; then
-            echo -e "${OK} The directory provided exists. Saving it to configuration file."
-            data_path=$(echo $user_input | sed 's/Storage/Data/')
-            save_settings "$user_input" "$data_path"
-            APP_PATH="$user_input"
+            echo -e "${OK} Directory exists. Saving configuration."
+            local data_path
+            data_path=$(echo "$user_input" | sed 's/Storage/Data/')
+            save_configuration "$user_input" "$data_path"
+            APP_CACHE_DIR="$user_input"
             break
         else
-            echo -e "${NOK} The directory provided does not exist. Please try again."
+            echo -e "${NOK} Directory does not exist. Try again."
         fi
     done
 }
 
-# Template for the 'settings.conf' file.
-save_settings() {
+# Save settings to the configuration file
+save_configuration() {
+    local cache_path="$1"
+    local data_path="$2"
     ensure_config_directory
-    echo -e "user_settings = {" > "$CONF_FILE"
-    echo -e "    app_name='spotify'," >> "$CONF_FILE"
-    echo -e "    cache_path='$1'," >> "$CONF_FILE"
-    echo -e "    data_path='$2'" >> "$CONF_FILE"
-    echo -e "}" >> "$CONF_FILE"
-    echo -e "${OK} Settings saved successfully in '$CONF_FILE'."
+    cat <<EOL >"$CONFIG_FILE"
+user_settings = {
+    app_name = 'spotify',
+    cache_path = '$cache_path',
+    data_path = '$data_path'
+}
+EOL
+    echo -e "${OK} Configuration saved."
 }
 
-# Check if the dependecies are installed. Do not alter the order below.
-echo -e "${OK} Ensuring the installation of dependencies."
+# Main script execution
+echo -e "${OK} Ensuring required dependencies are installed."
+check_gcc
 check_make
 check_lua
 check_luarocks
 check_luastatic
 
-# Check if the default cache directory exists. If not, let the user define one.
-check_directory
+echo -e "${OK} Checking application cache directory."
+check_cache_directory
 
 echo -e "${OK} Configuration completed successfully. Ready to proceed with the Makefile."
