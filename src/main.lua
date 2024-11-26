fabb06c96f7259e31b741af314e0ca87e8734c02
joseareia <jose.apareia@gmail.com>
2024-11-26 00:19:59 +0000

    fix: fetch the settings written in the settings file created in 356378c

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,28 @@
 local utils = require("src.utils")
 
+-- TODO: Deprecate these variables and the logic behind them. Does not make any sense!
 local is_installed = nil
 local app_name = 'spotify'
 local username = utils.get_username()
-local cache_path = '/home/' .. username .. '/.cache/spotify/Storage'
+
+-- Global variable that can be alter in the future.
+cache_path = '/home/' .. username .. '/.cache/spotify/Storage'
+
+-- Check if directory exists. 
+-- If not (default logic) a try-out to get into a custom setting file will be done.
+if utils.directory_exists(cache_path) == nil then
+    local config_env = {}
+    local home_env = os.getenv("HOME")
+    local f, err = loadfile(home_env .. "/.local/share/spotify-cleaner/settings.conf", "t", config_env)
+
+    if f then
+        f()
+        cache_path = config_env.user_settings.path
+        print("Loadfile " .. cache_path)
+    else
+        print(err)
+    end
+end
 
 -- Check is Spotify is installed.
 local handle = io.popen('which ' .. app_name)
@@ -15,7 +34,7 @@ else
     if result ~= "" then is_installed = true end
 end
 
--- If Spotify is installed, checks the cache size. If the cache exceeds 50MB, it is deleted.
+-- If Spotify is installed, checks the cache size. If the cache exceeds 100MB, it is deleted.
 if is_installed then
     local cache_exists = utils.directory_exists(cache_path)
     if cache_exists then cache_size = utils.get_directory_size(cache_path) end   
