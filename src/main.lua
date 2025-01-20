262d9a772aa97c7cc1014502d51de759f9902d9c
joseareia <jose.apareia@gmail.com>
2025-01-20 14:06:26 +0000

    feat: add log prints to monitor the tool behaviour

--- src/main.lua
+++ src/main.lua
@@ -7,6 +7,12 @@ local home_env = os.getenv("HOME")
 cache_path = home_env .. '/.cache/spotify/Storage'
 data_path = home_env .. '/.cache/spotify/Data'
 
+-- Current date and time. To be used in logs.
+current_time = os.date("%Y-%m-%dT%X")
+
+-- Limit cache size in MB.
+limit_cache_size = 100
+
 -- Check if directory exists. If not (default logic) a try-out to get into a custom file setting will be done.
 if utils.directory_exists(cache_path) == nil then
     local config_env = {}
@@ -21,28 +27,40 @@ if utils.directory_exists(cache_path) == nil then
     end
 end
 
--- Checks the cache size. If the cache exceeds 100MB, it is deleted.
+-- Checks the cache size. If the cache exceeds the given value, it is deleted.
 local cache_exists = utils.directory_exists(cache_path)
 if cache_exists then cache_size = utils.get_directory_size(cache_path) end   
 cache_size = tonumber(string.format("%.2f", cache_size / 1024)) -- Convert from kB to MB.
 
-if cache_size >= 100 then
+if cache_size >= limit_cache_size then
     local cmd_delete_dir = "rm -r " .. cache_path .. "/*"
     local ok, exit = os.execute(cmd_delete_dir)
     if ok ~= true then
-        print("[ ERROR ] Could not delete the directory due to: '" .. exit .. "'.")
+        print(current_time .. " spotify-cleaner: Cache was not cleared due to error: '" .. exit .. "'.")
+    else
+        print(current_time .. " spotify-cleaner: Cache was cleared (" .. cache_size .. "MB).")
     end
+else
+    print(current_time ..
+    " spotify-cleaner: Cache not cleared (" ..
+    cache_size .. "MB) as it is below the threshold (" .. limit_cache_size .. "MB).")
 end
 
--- Checks the data size. If the data exceeds 100MB, it is deleted.
+-- Checks the data size. If the cache exceeds the given value, it is deleted.
 local data_exists = utils.directory_exists(data_path)
 if data_exists then data_size = utils.get_directory_size(data_path) end
 data_size = tonumber(string.format("%.2f", data_size / 1024)) -- Convert from kB to MB.
 
-if data_size >= 100 then
+if data_size >= limit_cache_size then
     local cmd_delete_dir = "rm -r " .. data_path .. "/*"
     local ok, exit = os.execute(cmd_delete_dir)
     if ok ~= true then
-        print("[ ERROR ] Could not delete the directory due to: '" .. exit .. "'.")
+        print(current_time .. " spotify-cleaner: Data was not cleared due to error: '" .. exit .. "'.")
+    else
+        print(current_time .. " spotify-cleaner: Data was cleared (" .. data_size .. "MB).")
     end
+else
+    print(current_time ..
+    " spotify-cleaner: Dache not cleared (" ..
+    data_size .. "MB) as it is below the threshold (" .. limit_cache_size .. "MB).")
 end
